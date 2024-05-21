8871a677b0be5527e7cb3338dd5e18c24bd3521a
Kyungguk Min <min@kyungguk.com>
2024-05-21 10:14:39 +0900

    meta: Add a guard to path expansion

--- meta/run_pyspedas_ipython.sh
+++ meta/run_pyspedas_ipython.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-SPEDAS_DATA_DIR=$SCRIPT_DIR/../tmp ipython "$@"
+SPEDAS_DATA_DIR="${SCRIPT_DIR}/../tmp" ipython "$@"
